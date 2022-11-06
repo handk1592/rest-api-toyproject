1e6f1e7428c55beaf4d931d3952a84fedc2c4470
dreamhandg <handg0972@gmail.com>
2022-11-06 14:41:32 +0900

    init commit

---
+++ src/main/java/com/rest/edu/RestApiToyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.rest.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiToyprojectApplication.class, args);
+	}
+
+}
